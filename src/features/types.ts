e7d2c3760d40c8f3b496aa870f273bed7d821a0e
山口諒 <>
2023-01-28 00:17:22 +0900

    [WIP] type.ts Add READ_TASK

--- src/features/types.ts
+++ src/features/types.ts
@@ -28,9 +28,26 @@ export interface USER {
   id: number;
   username: string;
 }
-
 export interface AUTH_STATE {
   isLoginView: boolean;
   loginUser: LOGIN_USER;
   profiles: PROFILE[];
+}
+/* taskSlice.ts */
+export interface READ_TASK {
+  id: number;
+  task: string;
+  description: string;
+  criteria: string;
+  status: string;
+  status_name: string;
+  category: number;
+  category_item: string;
+  estimate: number;
+  responsible: number;
+  responsible_username: string;
+  owner: number;
+  owner_username: string;
+  created_at: number;
+  updated_at: number;
 }
